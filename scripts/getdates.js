3628348579d0f045dc4bc51c7d6cba912b9b7e0c
lmcreer <lisa.creer@gmail.com>
2025-01-26 21:29:21 -0500

    js test

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const today = new Date();
+
+//document.getElementById("currentyear").textContent=${today.getFullYear()};
+document.getElementById("currentyear").textContent="TESTING";
+document.querySelector("h3").textContent = "HELLO WORLD";
